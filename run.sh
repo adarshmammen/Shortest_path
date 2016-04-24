3a9c7d64dbd5eeef38be9acf5e6580177d4d050e
Adarsh Mammen <adarsh@Adarshs-MacBook-Pro.local>
2016-04-24 12:06:27 -0400

    upload to local host and display markers

--- run.sh
+++ run.sh
@@ -1,7 +1,19 @@
+
 #while inotifywait -e close_write myFile.txt; 
 #do
 ./geodesic
 ./project_algo
 python createXML.py
 rm myFile.txt nodes_for_xml.txt nodes_start_end.txt
+cp Landmarks.xml /var/www/html/.
+cp markers.html /var/www/html/.
+
+rm Landmarks.xml
+rm markers.html
+
+sudo chmod 777 /var/www/html/markers.html
+sudo chmod 777 /var/www/html/Landmarks.xml
+
+chromium-browser http://localhost/markers.html
+
 #done;
